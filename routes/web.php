0d80bc1615c9c150045d619d0cf83dc815160e4f
ahmadi97 <ahmadi.mh97@gmail.com>
2021-08-18 17:29:47 +0430

    optimize routes

--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,13 @@ Route::get('/adminpanel', function (){
     return view('admin.home');
 });
 
-Route::get('adminpanel/categories', [CategoryController::class, 'index'])->name('categories.index');
-Route::get('adminpanel/categories/create', [CategoryController::class, 'create'])->name('categories.create');
-Route::post('adminpanel/categories', [CategoryController::class, 'store'])->name('categories.store');
-Route::get('adminpanel/categories/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
-Route::patch('adminpanel/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
-Route::delete('adminpanel/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+
+
+Route::prefix('/adminpanel/categories')->name('categories.')->group(function (){
+    Route::get('', [CategoryController::class, 'index'])->name('index');
+    Route::get('/create', [CategoryController::class, 'create'])->name('create');
+    Route::post('', [CategoryController::class, 'store'])->name('store');
+    Route::get('/{category}/edit', [CategoryController::class, 'edit'])->name('edit');
+    Route::patch('/{category}', [CategoryController::class, 'update'])->name('update');
+    Route::delete('/{category}', [CategoryController::class, 'destroy'])->name('destroy');
+});
